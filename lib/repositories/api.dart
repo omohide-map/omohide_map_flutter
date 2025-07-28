8713220da810838b90f019af4166b5c5f4d61b26
naoya25 <onaoya2002@gmail.com>
2025-07-28 18:05:10 +0900

    feat: post, put, deleteのapiアクセス関数

--- lib/repositories/api.dart
+++ lib/repositories/api.dart
@@ -28,3 +28,92 @@ Future<Response<T>> getApi<T>(
     throw Exception(e.message);
   }
 }
+
+Future<Response<T>> postApi<T>(
+  String path, {
+  dynamic data,
+  Map<String, dynamic>? queryParameters,
+  Options? options,
+  CancelToken? cancelToken,
+  ProgressCallback? onSendProgress,
+  ProgressCallback? onReceiveProgress,
+  Duration? timeout,
+}) async {
+  final url = '$apiBaseUrl$path';
+  final dio = Dio();
+
+  try {
+    final response = await dio
+        .post<T>(
+          url,
+          data: data,
+          queryParameters: queryParameters,
+          options: options,
+          cancelToken: cancelToken,
+          onSendProgress: onSendProgress,
+          onReceiveProgress: onReceiveProgress,
+        )
+        .timeout(timeout ?? const Duration(seconds: 10));
+    return response;
+  } on DioException catch (e) {
+    throw Exception(e.message);
+  }
+}
+
+Future<Response<T>> putApi<T>(
+  String path, {
+  dynamic data,
+  Map<String, dynamic>? queryParameters,
+  Options? options,
+  CancelToken? cancelToken,
+  ProgressCallback? onSendProgress,
+  ProgressCallback? onReceiveProgress,
+  Duration? timeout,
+}) async {
+  final url = '$apiBaseUrl$path';
+  final dio = Dio();
+
+  try {
+    final response = await dio
+        .put<T>(
+          url,
+          data: data,
+          queryParameters: queryParameters,
+          options: options,
+          cancelToken: cancelToken,
+          onSendProgress: onSendProgress,
+          onReceiveProgress: onReceiveProgress,
+        )
+        .timeout(timeout ?? const Duration(seconds: 10));
+    return response;
+  } on DioException catch (e) {
+    throw Exception(e.message);
+  }
+}
+
+Future<Response<T>> deleteApi<T>(
+  String path, {
+  dynamic data,
+  Map<String, dynamic>? queryParameters,
+  Options? options,
+  CancelToken? cancelToken,
+  Duration? timeout,
+}) async {
+  final url = '$apiBaseUrl$path';
+  final dio = Dio();
+
+  try {
+    final response = await dio
+        .delete<T>(
+          url,
+          data: data,
+          queryParameters: queryParameters,
+          options: options,
+          cancelToken: cancelToken,
+        )
+        .timeout(timeout ?? const Duration(seconds: 10));
+    return response;
+  } on DioException catch (e) {
+    throw Exception(e.message);
+  }
+}

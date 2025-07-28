d551fc467b7763634f4ecc1140b0f4c85895463b
naoya25 <onaoya2002@gmail.com>
2025-07-28 18:31:05 +0900

    wip

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -2,14 +2,14 @@ import 'package:envied/envied.dart';
 
 part 'env.g.dart';
 
-@Envied(requireEnvFile: true)
+@Envied(requireEnvFile: true, obfuscate: true)
 final class Env {
   @EnviedField(varName: 'GOOGLE_IOS_CLIENT_ID')
-  static const String googleIosClientId = _Env.googleIosClientId;
+  static String googleIosClientId = _Env.googleIosClientId;
 
   @EnviedField(varName: 'SUPABASE_URL')
-  static const String supabaseUrl = _Env.supabaseUrl;
+  static String supabaseUrl = _Env.supabaseUrl;
 
   @EnviedField(varName: 'SUPABASE_ANON_KEY')
-  static const String supabaseAnonKey = _Env.supabaseAnonKey;
+  static String supabaseAnonKey = _Env.supabaseAnonKey;
 }
